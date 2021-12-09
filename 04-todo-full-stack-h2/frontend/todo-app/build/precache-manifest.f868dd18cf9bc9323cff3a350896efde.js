7e4b18ef0498c5ab9dba703615a11f7d92876e78
Sony Mathew <mailsonymathew@gmail.com>
2021-12-09 16:45:18 +0530

    Dec09_2021

---
+++ 04-todo-full-stack-h2/frontend/todo-app/build/precache-manifest.f868dd18cf9bc9323cff3a350896efde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9550753b618c6c48ef6",
+    "url": "/static/css/main.1b85faf5.chunk.css"
+  },
+  {
+    "revision": "e9550753b618c6c48ef6",
+    "url": "/static/js/main.d59e66bb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "829f7c9570ea8ec8522d",
+    "url": "/static/js/2.faa474ff.chunk.js"
+  },
+  {
+    "revision": "8bdcc20d7fee4717c5fa8620f3b271e6",
+    "url": "/index.html"
+  }
+];
